5ec4337b8c80ba41f5a3bfd3f702fec818fa56fc
inesmtibaa <ines.mtibaa@insat.ucar.tn>
2026-03-19 21:55:04 +0100

    ines s work

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "./*.html",
+    "./pages/**/*.html",
+    "./js/**/*.js"
+  ],
   theme: {
     extend: {},
   },
