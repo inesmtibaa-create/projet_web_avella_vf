5a51a4e393277844069d44757f684272a4922bc2
sghaierchahd9-cmd <sghaierchahd9@gmail.com>
2026-03-27 16:44:23 +0100

    pearls and sha and shoes boutiques done

---
+++ js/pearls_products.js
@@ -0,0 +1,54 @@
+const products= [
+{
+  id:1,
+  name: "sac à main",
+  price: "120,000 TND",
+  description: "Structure rigide ,touche bohème et élégante ",
+  image: "../accesoires/sac.png",
+  colors: [
+{ name: "Beige", hex: "#c08352", image: "../accesoires/sac.png" },
+{ name: "Noir", hex: "#080808", image: "../accesoires/sac_noire.png" }
+]
+},
+ { id:2,
+  name: "Ceinture femme",
+  price: "45,000 TND",
+  description: " ceinture avec une touche naturelle et tendance. ",
+  image: "../accesoires/ceinture.png",
+  colors: [
+  {name: "Beige clair", hex: "#dbc5b4", image: "../accesoires/ceinture_bc.png" },
+  {name: "beige", hex: "#ecae5d", image: "../accesoires/ceinture_b.png"},
+  {name: "noir", hex: "#1a1a1a", image: "../accesoires/ceinture_n.png"}
+ ]
+  },
+   { id:3,
+name: "boucles fines",
+price: "25,000 TND",
+description: " boucles en crystal ",
+image: "../accesoires/boucles.png",
+colors: [
+{name: "noire", hex: "#131211", image: "../accesoires/boucles.png" }
+]},
+  { id:4,
+ name: "Collection des boucles",
+ price: "35,000 TND",
+ description: " boucles tendances avec une touche naturelle ",
+ image: "../accesoires/collection.png",
+ colors: [
+ {name: "orange", hex: "#fe671b", image: "../accesoires/boucle_orange.png" },
+ {name: "beige", hex: "#ecae5d", image: "../accesoires/boucles_b.png"},
+ {name: "rose", hex: "#ae7979", image: "../accesoires/boucles_rose.png"}
+ ]
+},
+  { id:5,
+ name: "Colliers en perles",
+ price: "25,000 TND",
+ description: " design délicat et lumineux ",
+ image: "../accesoires/colliers.png",
+ colors: [
+ {name: "blanc", hex: "#e6e2e0", image: "../accesoires/colliers.png" },
+ ]
+}
+
+
+]
---
+++ js/shoes_products.js
@@ -0,0 +1,47 @@
+const products= [
+{
+  id:1,
+  name: "Mule Femme ",
+  price: "120,000 TND",
+  description: "Mules elegantes, confortables et chic",
+  image: "../shoes/mule.png",
+  colors: [
+{ name: "Beige", hex: "#e1c9b5", image: "../shoes/mule.png" },
+{ name: "Noir", hex: "#080808", image: "../shoes/mule_noire.png" }
+]},
+  {id:2,
+  name: "High Heels",
+  price: "85,000 TND",
+  description: "Escarpins raffinés à bout pointu ",
+  image: "../shoes/women.png",
+  colors: [
+{ name: "Beige", hex: "#dcc2ac", image: "../shoes/women.png" },
+{ name: "marron", hex: "#473527", image: "../shoes/marron.png"}
+]
+
+},
+  {id:3,
+  name: "chaussures homme",
+  price: "75,000 TND",
+  description: "chaussures en dain",
+  image: "../shoes/homme.png",
+  colors: [
+ { name: "Beige", hex: "#dcc2ac", image: "../shoes/homme.png" },
+  { name: "marron", hex: "#161134", image: "../shoes/homme_blue.png"}
+ ]
+
+
+},
+ {id:4,
+ name: "chaussures bébé",
+ price: "80,000 TND",
+ description: "chaussures en dain",
+ image: "../shoes/bebe.png",
+ colors: [
+{ name: "marron", hex: "#b27340", image: "../shoes/bebe.png" },
+]
+ }
+
+
+
+]
